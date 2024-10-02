91931ad4bd972c8b70b5f1f6cb21181692a53c08
CalumH-Haystack <calumhay@haystackdev.au>
2024-10-02 21:37:49 +1000

    Removed Splash Screen

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,1 @@
 import './global.css';
-
-export const onClientEntry = () => {
-	if (document.getElementById('splash-screen')) {
-		document.getElementById('splash-screen').style.display = 'flex';
-	}
-};
-
-export const onInitialClientRender = () => {
-	if (document.getElementById('splash-screen')) {
-		document.getElementById('splash-screen').style.display = 'flex';
-	}
-};
-
-export const onRouteUpdate = () => {
-	if (document.getElementById('splash-screen')) {
-		document.getElementById('splash-screen').style.display = 'none';
-	}
-};
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,38 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { ThemeProvider } from '@emotion/react';
-import SplashScreen from '../../components/SplashScreen';
 import { theme } from '../../theme';
 import Page from '../Page';
+import { useLocation } from '@reach/router';
 
 interface IAppProps {
 	children?: React.ReactNode;
 }
 
 function App({ children }: IAppProps) {
-	const [isLoading, setIsLoading] = useState(true);
-	const [fadeSplash, setFadeSplash] = useState(false);
-
-	useEffect(() => {
-		const checkFontsLoaded = async () => {
-			await document && document.fonts.ready;
-
-			setTimeout(() => {
-				setFadeSplash(true);
-				setTimeout(() => setIsLoading(false), 500);
-			}, 1000);
-		};
-		checkFontsLoaded().catch(console.error);
-	}, [setIsLoading]);
 
 	return (
 		<ThemeProvider theme={theme}>
-			{isLoading && <SplashScreen fade={fadeSplash} />}
 			<div className='App'>
 				<header className='App-header'>
-					<Page>
-						{children}
-					</Page>
+					<Page>{children}</Page>
 				</header>
 			</div>
 		</ThemeProvider>

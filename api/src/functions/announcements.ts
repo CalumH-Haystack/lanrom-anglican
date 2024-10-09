212b2c87300b28641db02b664a1972cc0eedd4e8
CalumH-Haystack <calumhay@haystackdev.au>
2024-10-09 19:16:43 +1100

    Set up announcements function to retrieve urls

--- api/src/functions/announcements.ts
+++ api/src/functions/announcements.ts
@@ -1,15 +1,51 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+	app,
+	HttpRequest,
+	HttpResponseInit,
+	InvocationContext
+} from '@azure/functions';
+import { BlobServiceClient } from '@azure/storage-blob';
 
-export async function announcements(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function announcements(
+	request: HttpRequest,
+	context: InvocationContext
+): Promise<HttpResponseInit> {
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    const urls: Array<string> = [];
+    
+	try {
+		const blobServiceClient = new BlobServiceClient(
+			`https://lanromstorage.blob.core.windows.net`
+		);
 
-    return { body: `Hello, ${name}!` };
-};
+		const containerName = 'announcements';
+		const containerClient = await blobServiceClient.getContainerClient(
+			containerName
+		);
+
+		for await (const blob of containerClient.listBlobsFlat()) {
+			// blob
+			console.log('\t', blob.name);
+
+			// Get Blob Client from name, to get the URL
+			const tempBlockBlobClient = containerClient.getBlockBlobClient(blob.name);
+
+			// Push blob URL
+            urls.push(tempBlockBlobClient.url);
+		}
+	} catch {
+		e => {
+			context.log(e);
+		};
+	}
+
+	return { jsonBody: {
+        urls
+    } };
+}
 
 app.http('announcements', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: announcements
+	methods: ['GET'],
+	authLevel: 'anonymous',
+	handler: announcements
 });

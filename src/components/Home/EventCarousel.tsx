818a1d2830cbd588f840db38c78de3972a335c00
CalumH-Haystack <calumhay@haystackdev.au>
2024-10-15 16:20:32 +1100

    Added az announcements api url to env

--- src/components/Home/EventCarousel.tsx
+++ src/components/Home/EventCarousel.tsx
@@ -1,11 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
-import { Paper, useTheme, useMediaQuery } from '@mui/material';
+import { Paper, useTheme } from '@mui/material';
 import axios from 'axios';
 import { GatsbyImage } from "gatsby-plugin-image"
 
 export const EventCarousel = () => {
-	const theme = useTheme();
 	const [imageUrls, setImageUrls] = useState(new Array<string>());
 
 	const fetchUrls = async () => {
@@ -67,20 +66,3 @@ export const EventCarousel = () => {
 		</>
 	);
 };
-
-type Item = {
-	alt?: string;
-	href: string;
-};
-
-const items: Item[] = [
-	{
-		href: 'https://static.wixstatic.com/media/07894e_b417c8deaa0b4754830283c42f4e663d~mv2.jpg/v1/fill/w_952,h_517,al_c,q_85,enc_auto/1724222922196-8389626f-d50d-4108-aa44-12f358ae8961_1.jpg'
-	},
-	{
-		href: 'https://static.wixstatic.com/media/07894e_feadba2b0b2b41ee8f0cd70ec08b443a~mv2.jpg/v1/fill/w_1129,h_802,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2024%20Christianity%20explored%20flyer.jpg'
-	},
-	{
-		href: 'https://static.wixstatic.com/media/718c95_37e54d4f3aa54da4a4505e2978acc9bb~mv2.jpg/v1/fill/w_484,h_296,q_90/718c95_37e54d4f3aa54da4a4505e2978acc9bb~mv2.webp'
-	}
-];

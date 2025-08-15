a7cc80cbd2957d0282dc78c67a11ef0ed13b3186
Calum Hay <calumhay@haystackdev.au>
2025-08-15 13:09:36 +1000

    Added seo data

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,7 +7,9 @@ require("dotenv").config({
 const config: GatsbyConfig = {
 	siteMetadata: {
 		title: `Anglican Parish of Lancefield with Romsey`,
-		siteUrl: `https://www.lancefieldromseyanglican.org/`
+		description: "Connecting with God in our community",
+		siteUrl: `https://www.lancefieldromseyanglican.org/`,
+		image: `/lanRomThumbnail.png`
 	},
 	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
 	// If you use VSCode you can also use the GraphQL plugin

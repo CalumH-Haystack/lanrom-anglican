0094aef446012d236353700323a3b2c9812533a0
Calum Hay <calumhay@haystackdev.au>
2025-08-21 10:30:48 +1000

    Added images

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -3,10 +3,27 @@ import App from '../../components/App/App';
 import { HeadFC } from 'gatsby';
 import { ContactForm } from '../../components/Contact/components';
 import { Heading, Paragraph } from '../../utils';
+import { Box } from '@mui/material';
+import { StaticImage } from 'gatsby-plugin-image';
+import { BOX_SHADOW } from '../../theme/palette';
 
 const Contact = () => {
 	return (
 		<App>
+			<Box
+				sx={{
+					boxShadow: BOX_SHADOW,
+					marginBottom: '16px',
+					width: '100%'
+				}}
+			>
+				<StaticImage
+					src='../../images/socialsBanner.jpg'
+					alt='Aerial view of Christ Church Lancefield'
+					placeholder='blurred'
+					layout='constrained'
+				/>
+			</Box>
 			<Heading variant='h1'>Contact Us</Heading>
 			<Heading variant='h2'>St Pauls Romsey</Heading>
 			<Paragraph>77 Main St, Romsey, VIC</Paragraph>
